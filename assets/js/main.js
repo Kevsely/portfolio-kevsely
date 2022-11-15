78b75d9da0271f158df52eec9361b0fe6ca26c36
Kevsely <elegis.sossou@yahoo.com>
2022-11-15 21:28:49 +0100

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,28 +215,29 @@
         msg = $.trim(msg);
 
         if (name != '' && email != '' && msg != '') {
-          var values = "name=" + name +
-            "&subject=" + subject +
-            "&phone=" + phone +
-            "&email=" + email +
-            "&msg=" + msg;
-          $.ajax({
-            type: "POST",
-            url: "assets/php/mail.php",
-            data: values,
-            success: function () {
-              $('#name').val('');
-              $('#subject').val('');
-              $('#phone').val('');
-              $('#email').val('');
-              $('#msg').val('');
-
               $('#st-alert').fadeIn().html('<div class="alert alert-success"><strong>Success!</strong> Email has been sent successfully.</div>');
               setTimeout(function () {
                 $('#st-alert').fadeOut('slow');
               }, 4000);
-            }
-          });
+
+          // var values = "name=" + name +
+          //   "&subject=" + subject +
+          //   "&phone=" + phone +
+          //   "&email=" + email +
+          //   "&msg=" + msg;
+          // $.ajax({
+          //   type: "POST",
+          //   url: "assets/php/mail.php",
+          //   data: values,
+          //   success: function () {
+          //     $('#name').val('');
+          //     $('#subject').val('');
+          //     $('#phone').val('');
+          //     $('#email').val('');
+          //     $('#msg').val('');
+
+          //   }
+          // });
         } else {
           $('#st-alert').fadeIn().html('<div class="alert alert-danger"><strong>Warning!</strong> All fields are required.</div>');
         }
