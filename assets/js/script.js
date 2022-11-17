abf58dd8312fa93717943e90f3c8c29ecbfb2869
Kevsely <elegis.sossou@yahoo.com>
2022-11-17 10:08:02 +0100

    Updated contact form submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,33 @@
-const contactForm = document.getElementById('contact-form')
-const succesAlert = document.querySelector('#st-alert')
+const contactForm = document.getElementById("contact-form");
+const succesAlert = document.querySelector("#st-alert");
+
+(function () {
+  emailjs.init("Hr4OQd2ji2m_zYlUH");
+})();
 
 function sendEmail() {
-    const senderName = contactForm[0].value
-    const senderEmail = contactForm[1].value
-    const msgSubject = contactForm[2].value
-    const msgContent = contactForm[3].value
-    
-    // Sending the form by email
-    if (senderName != '' && senderEmail != '' && msgContent != '')
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Port : 2525,
-        Username : "skybet229@gmail.com",
-        Password : "C117DBF7C058BF45B988F518F358129EF7ED",
-        To : 'elegis.sossou@yahoo.com',
-        From : 'skybet229@gmail.com',
-        Subject : "New message from your Portfolio contact form",
-        Body : `From: ${senderName}
-        Email: ${senderEmail}
-        Subject: ${msgSubject}
-        Message: ${msgContent}`
-    }).then(() => {
-        contactForm[0].value = ''
-        contactForm[1].value = ''
-        contactForm[2].value = ''
-        contactForm[3].value = ''
-    }
-    );
+  const senderName = contactForm[0].value;
+  const senderEmail = contactForm[1].value;
+  const msgSubject = contactForm[2].value;
+  const msgContent = contactForm[3].value;
+
+  const templateParams = {
+    senderName,
+    senderEmail,
+    msgSubject,
+    msgContent,
+  };
+
+  // Sending the form by email
+  if (senderName != "" && senderEmail != "" && msgContent != "")
+    emailjs
+      .send("service_kov6bvv", "template_3mo25b9", templateParams)
+      .then(() => {
+        contactForm[0].value = "";
+        contactForm[1].value = "";
+        contactForm[2].value = "";
+        contactForm[3].value = "";
+
+        console.log("Message sent sucessfully");
+      });
 }
